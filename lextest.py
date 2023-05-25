35dd14cc17f7870052d280effa0f05021b1d056c
Telmo Maciel <telmomaciel9@gmail.com>
2023-05-25 22:52:06 +0100

    gramática feita

--- lextest.py
+++ lextest.py
@@ -4,9 +4,7 @@ import toml
 
 
 tokens = (
-    #'LPAREN',
     'OBJECT',
-    #'RPAREN',
     'KEY',
     'STRING',
     'COMMENT',
@@ -17,22 +15,19 @@ tokens = (
     'BOOLEAN'
 )
 
-literals = '-:.",[]'
+literals = '-:.",=[]'
 
-t_ignore = ' \t\n=,[]'
+t_ignore = ' \t\n'
 
 def t_BOOLEAN(t): r'TRUE|FALSE|true|false'; return t
 def t_IP(t): r'\"\d+\.\d+\.\d+\.\d+\"'; return t
 def t_DATE(t): r'\d+\-\d+\-\d+'; return t
 def t_TIME(t): r'\d+\:\d+\:\d+'; return t
 def t_NUM(t): r'\d+'; return t
-def t_COMMENT(t): r'\#\s.*'; return t
+def t_COMMENT(t): r'(\#\s.*)'; return t
 def t_KEY(t): r'[a-z]+\D?[a-z]+\s'; return t
-#def t_LPAREN(t): r'\[' ; return t
-def t_STRING(t): r'\".+\"'; return t
+def t_STRING(t): r'(\"\"\"|\'\'\'|\'|\")(.*?)(\"\"\"|\'\'\'|\'|\")'; return t
 def t_OBJECT(t): r'[a-z]*\.?[a-z]+'; return t
-#def t_RPAREN(t): r'\]' ; return t
-
 
 
 def t_error(t):
@@ -51,4 +46,4 @@ with open("exemplo1.toml",'r') as file:
         tok = lexer.token()
         if not tok:
             break
-        print(tok)
+        #print(tok)
--- yacctest.py
+++ yacctest.py
@@ -1,14 +1,16 @@
-
+import sys
+from lextest import tokens, texto
+import ply.yacc as yacc
 
 # T   : {'=', '[', ']', ',', KEY, OBJECT, STRING, BOOLEAN, IP, 
 #        NUM, DATE, TIME, COMMENT}
 # NT  : {FT, Linha, Atributo, Objeto, Valor, List, LCont, Conteudo, Cont2}
 
 # P1  : FT : Linha FT 
-# P2  :    | Linha
-# P3  : Linha : Atributo Linha
+# P2  :    | 
+# P3  : Linha : Atributo 
 # P4  :       | Objeto 
-# P5  :       | 
+# P5  :       | COMMENT
 # P6  : Atributo : KEY '=' Valor
 # P7  : Objeto : '[' OBJECT ']' Linha
 # P8  : Valor : STRING
@@ -18,74 +20,127 @@
 # P12 :       | DATE
 # P13 :       | TIME
 # P14 :       | List
-# P15 :       | COMMENT
-# P16 : List : '[' LCont
-# P17 : LCont : ']'
-# P18 :       | Conteudo ']'
-# P19 : Conteudo : Valor Cont2
-# P20 : Cont2 : ',' Conteudo
-# P21 :       |
+# P15 : List : '[' LCont
+# P16 : LCont : ']'
+# P17 :       | Conteudo ']'
+# P18 : Conteudo : Valor Cont2
+# P19 : Cont2 : ',' Conteudo
+# P20 :       |
 
 
-def p_FT1(t):
+def p_FT1(p):
     "FT : Linha FT "
+    #print("Reconheci uma linha:", p[1])
+    p[0] = p[1] + p[2]
 
-def p_FT2(t):
-    "FT : Linha"
+def p_FT2(p):
+    "FT : "
+    p[0] = ""
 
-def p_Linha1(t):
-    "Linha : Atributo Linha"
+def p_Linha1(p):
+    "Linha : Atributo"
+    print("Atributo:", p[1])
+    p[0] = p[1] 
 
-def p_Linha2(t):
+def p_Linha2(p):
     "Linha : Objeto"
+    print("Objeto:", p[1])
+    p[0] = p[1]
 
-def p_Linha3(t):
-    "Linha : "
+def p_Linha3(p):
+    "Linha : COMMENT"
+    print("Reconheci um COMMENT:", p[1])
+    p[0] = p[1]
 
-def p_Atributo(t):
+def p_Atributo(p):
     "Atributo : KEY '=' Valor"
+    #print("key:", p[1], ", valor:", p[3])
+    #FUNCIONA
+    p[0] = p[1] + ': ' + p[3]
 
-def p_Objeto(t):
-    "Objeto : '[' OBJECT ']' Linha"
+def p_Objeto(p):
+    "Objeto : '[' OBJECT ']'"
+    #print("Reconheci um object: ", p[2])
+    p[0] = p[1] + p[2] + p[3]
 
-def p_Valor1(t):
+def p_Valor1(p):
     "Valor : STRING"
+    #print("Reconheci uma string", p[1])
+    #FUNCIONA
+    p[0] = p[1]
 
-def p_Valor2(t):
+def p_Valor2(p):
     "Valor : BOOLEAN"
+    #print("Reconheci um bool", p[1])
+    #FUNCIONA
+    p[0] = p[1]
 
-def p_Valor3(t):
+def p_Valor3(p):
     "Valor : IP"
+    #print("Reconheci um IP", p[1])
+    #FUNCIONA
+    p[0] = p[1]
 
-def p_Valor4(t):
+def p_Valor4(p):
     "Valor : NUM"
+    #print("Reconheci um NUM", p[1])
+    #FUNCIONA
+    p[0] = p[1]
 
-def p_Valor5(t):
+def p_Valor5(p):
     "Valor : DATE"
+    #print("Reconheci uma DATA", p[1])
+    #FUNCIONA
+    p[0] = p[1] 
 
-def p_Valor6(t):
+def p_Valor6(p):
     "Valor : TIME"
+    #print("Reconheci TIME", p[1])
+    #FUNCIONA
+    p[0] = p[1]
 
-def p_Valor7(t):
-    "Valor : LIST"  
+def p_Valor7(p):
+    "Valor : List"
+    #print("Reconheci a lista:", p[1])
+    #FUNCIONA
+    p[0] = p[1]                       
 
-def p_Valor8(t):
-    "Valor : COMMENT"                      
-
-def p_List(t):
+def p_List(p):
     "List : '[' LCont"
+    #FUNCIONA
+    p[0] = p[1] + p[2] 
 
-def p_LCont1(t):
+def p_LCont1(p):
     "LCont : ']'"
+    #FUNCIONA
+    p[0] = p[1]
 
-def p_LCont2(t):
+def p_LCont2(p):
     "LCont : Conteudo ']'"
+    #FUNCIONA
+    p[0] = p[1] + p[2] 
 
-def p_Conteudo(t):
+def p_Conteudo(p):
     "Conteudo : Valor Cont2"
+    #FUNCIONA
+    p[0] = p[1] + p[2]
 
-def p_Cont2_1(t):
+def p_Cont2_1(p):
     "Cont2 : ',' Conteudo"
+    #FUNCIONA
+    p[0] = ',' + p[2] 
 
-def p_Cont2_2(t):
+def p_Cont2_2(p):
     "Cont2 : "
+    #FUNCIONA
+    p[0] = ""
+
+def p_error(p):
+    parser.success = False
+    print(f"Sintax error: {p.value}")
+
+
+parser = yacc.yacc()
+parser.success = True
+
+parser.parse(texto)
